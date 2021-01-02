14a1615dca0927357fb51da877c62a8424722645
José <joseitamonte@hotmail.com>
2021-01-02 10:57:49 -0300

    Add organize by regex

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from os.path import isfile, join
 import shutil
 import json
 import time
+import re
 
 def getFiles(path):
   files = [f for f in listdir(path) if isfile(join(path, f))]
@@ -12,6 +13,10 @@ def getRules():
   with open('rules.json') as json_file:
     rules = json.load(json_file)
     return rules
+
+def moveFiles(sourcePath, destinationPath):
+  new_path = shutil.move(sourcePath, destinationPath)
+  print(new_path)
 
 def organizeByExtension(files, rules, source):
   for f in files:
@@ -26,20 +31,35 @@ def organizeByExtension(files, rules, source):
       continue    
     sourcePath = source + f
     destinationPath = moveTo + f
-    new_path = shutil.move(sourcePath, destinationPath)
-    print(new_path)
+    moveFiles(sourcePath, destinationPath)
+
+def organizeByRegex(files, rules, source):
+  for f in files:
+    for r in rules:
+      x = re.search(r[0], f)
+      if(x == None):
+        continue
+      sourcePath = source + f
+      destinationPath = r[1] + f
+      moveFiles(sourcePath, destinationPath)
 
 def mapRules(rules):
-  r = {}
+  extensionRules = {}
+  regexRules = []
   for rule in rules['rules']:
-    for e in rule['extensions']:
-      r[e] = rule['moveTo']
-  return r
+    try:
+      for e in rule['extensions']:
+        extensionRules[e] = rule['moveTo']
+    except:
+      regexRules.append([rule['regex'], rule['moveTo']])      
+  return extensionRules, regexRules
 
 def fileOrganizer():
   rules = getRules()
   files = getFiles(rules['source'])
-  organizeByExtension(files, mapRules(rules), rules['source'])
+  extensionRules, regexRules = mapRules(rules)
+  organizeByRegex(files, regexRules, rules['source'])
+  organizeByExtension(files, extensionRules, rules['source'])
 
 if __name__ == "__main__":
   while(True):
